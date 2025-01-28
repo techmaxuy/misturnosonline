446a60b12adff1054a803a7fbc532522b30071d0
Maximiliano DUva <techmax@adinet.com.uy>
2025-01-28 17:45:19 -0300

    seedUsers

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -4,30 +4,29 @@ import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
 const client = await db.connect();
 
-// async function seedUsers() {
-//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-//   await client.sql`
-//     CREATE TABLE IF NOT EXISTS users (
-//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       name VARCHAR(255) NOT NULL,
-//       email TEXT NOT NULL UNIQUE,
-//       password TEXT NOT NULL
-//     );
-//   `;
+async function seedUsers() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+ await client.sql`
+   CREATE TABLE IF NOT EXISTS users (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL
+    );
+  `;
 
-//   const insertedUsers = await Promise.all(
-//     users.map(async (user) => {
-//       const hashedPassword = await bcrypt.hash(user.password, 10);
-//       return client.sql`
-//         INSERT INTO users (id, name, email, password)
-//         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-//         ON CONFLICT (id) DO NOTHING;
-//       `;
-//     }),
-//   );
-
-//   return insertedUsers;
-// }
+  const insertedUsers = await Promise.all(
+    users.map(async (user) => {
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+     return client.sql`
+       INSERT INTO users (id, name, email, password)
+       VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+       ON CONFLICT (id) DO NOTHING;
+     `;
+    }),
+  );
+   return insertedUsers;
+}
 
 // async function seedInvoices() {
 //   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
