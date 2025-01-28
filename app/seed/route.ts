837cbe4024a819f24b0d6543d0fc207d756c4fa8
Maximiliano DUva <techmax@adinet.com.uy>
2025-01-28 17:51:35 -0300

    decomento total

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -28,77 +28,77 @@ async function seedUsers() {
    return insertedUsers;
 }
 
-// async function seedInvoices() {
-//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+async function seedInvoices() {
+   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-//   await client.sql`
-//     CREATE TABLE IF NOT EXISTS invoices (
-//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       customer_id UUID NOT NULL,
-//       amount INT NOT NULL,
-//       status VARCHAR(255) NOT NULL,
-//       date DATE NOT NULL
-//     );
-//   `;
+   await client.sql`
+     CREATE TABLE IF NOT EXISTS invoices (
+       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+       customer_id UUID NOT NULL,
+       amount INT NOT NULL,
+       status VARCHAR(255) NOT NULL,
+       date DATE NOT NULL
+    );
+   `;
 
-//   const insertedInvoices = await Promise.all(
-//     invoices.map(
-//       (invoice) => client.sql`
-//         INSERT INTO invoices (customer_id, amount, status, date)
-//         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-//         ON CONFLICT (id) DO NOTHING;
-//       `,
-//     ),
-//   );
+   const insertedInvoices = await Promise.all(
+     invoices.map(
+       (invoice) => client.sql`
+         INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+         ON CONFLICT (id) DO NOTHING;
+       `,
+     ),
+   );
 
-//   return insertedInvoices;
-// }
+   return insertedInvoices;
+ }
 
-// async function seedCustomers() {
-//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+ async function seedCustomers() {
+   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-//   await client.sql`
-//     CREATE TABLE IF NOT EXISTS customers (
-//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       name VARCHAR(255) NOT NULL,
-//       email VARCHAR(255) NOT NULL,
-//       image_url VARCHAR(255) NOT NULL
-//     );
-//   `;
+   await client.sql`
+     CREATE TABLE IF NOT EXISTS customers (
+       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+       name VARCHAR(255) NOT NULL,
+       email VARCHAR(255) NOT NULL,
+       image_url VARCHAR(255) NOT NULL
+     );
+   `;
 
-//   const insertedCustomers = await Promise.all(
-//     customers.map(
-//       (customer) => client.sql`
-//         INSERT INTO customers (id, name, email, image_url)
-//         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-//         ON CONFLICT (id) DO NOTHING;
-//       `,
-//     ),
-//   );
+   const insertedCustomers = await Promise.all(
+     customers.map(
+       (customer) => client.sql`
+         INSERT INTO customers (id, name, email, image_url)
+        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+         ON CONFLICT (id) DO NOTHING;
+       `,
+     ),
+   );
 
-//   return insertedCustomers;
-// }
+  return insertedCustomers;
+ }
 
-// async function seedRevenue() {
-//   await client.sql`
-//     CREATE TABLE IF NOT EXISTS revenue (
-//       month VARCHAR(4) NOT NULL UNIQUE,
-//       revenue INT NOT NULL
-//     );
-//   `;
+ async function seedRevenue() {
+   await client.sql`
+     CREATE TABLE IF NOT EXISTS revenue (
+       month VARCHAR(4) NOT NULL UNIQUE,
+       revenue INT NOT NULL
+     );
+   `;
 
-//   const insertedRevenue = await Promise.all(
-//     revenue.map(
-//       (rev) => client.sql`
-//         INSERT INTO revenue (month, revenue)
-//         VALUES (${rev.month}, ${rev.revenue})
-//         ON CONFLICT (month) DO NOTHING;
-//       `,
-//     ),
-//   );
+   const insertedRevenue = await Promise.all(
+     revenue.map(
+       (rev) => client.sql`
+         INSERT INTO revenue (month, revenue)
+         VALUES (${rev.month}, ${rev.revenue})
+         ON CONFLICT (month) DO NOTHING;
+      `,
+     ),
+   );
 
-//   return insertedRevenue;
-// }
+   return insertedRevenue;
+ }
 
 export async function GET() {
     //return Response.json({
@@ -107,10 +107,10 @@ export async function GET() {
     //});
      try {
        await client.sql`BEGIN`;
-    //   await seedUsers();
-    //   await seedCustomers();
-    //   await seedInvoices();
-    //   await seedRevenue();
+       await seedUsers();
+      await seedCustomers();
+       await seedInvoices();
+       await seedRevenue();
      await client.sql`COMMIT`;
   
       return Response.json({ message: 'Database seeded successfully' });
