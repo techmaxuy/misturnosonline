605289e77c850644df834faf4760931a4a3f95c3
Maximiliano DUva <techmax@adinet.com.uy>
2025-01-28 16:47:25 -0300

    botones mantienen estado

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -1,11 +1,15 @@
+'use client';
+
 import {
     CalendarIcon,
     HomeIcon,
     CalendarDaysIcon,
     CreditCardIcon,
     IdentificationIcon,
-  } from '@heroicons/react/20/solid';
+  } from '@heroicons/react/24/outline';
   import Link from 'next/link';
+  import { usePathname } from 'next/navigation';
+  import clsx from 'clsx';
    
   const links = [
     { icon: HomeIcon, name: 'Home', href: '/' },
@@ -16,6 +20,7 @@ import {
   ];
 
   export default function NavLinks() {
+    const pathname = usePathname();
     return (
       <>
         
@@ -25,7 +30,12 @@ import {
                 <Link
                 key={link.name}
                 href={link.href}
-                className="flex h-[48px] items-center justify-center grow gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+                className={clsx(
+                    'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                    {
+                      'bg-sky-100 text-blue-600': pathname === link.href,
+                    },
+                  )}
                 >
                 <LinkIcon className="w-6" />
                 <p className="hidden md:block">{link.name}</p>
