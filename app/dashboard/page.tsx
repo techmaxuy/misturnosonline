bc54002154f11991f9bc9cdd26a4a9aba0b37f79
Maximiliano DUva <techmax@adinet.com.uy>
2025-01-29 12:11:35 -0300

    nuevo comentarios por componentes no usados

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,8 +3,8 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {
-    fetchRevenue,
-    fetchLatestInvoices,
+    //fetchRevenue,
+    //fetchLatestInvoices,
     fetchCardData,
   } from '@/app/lib/data';
  
