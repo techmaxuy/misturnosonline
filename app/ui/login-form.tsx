dc7ae19005416bdf52f734dfe3a2c68d75b392a1
Maximiliano DUva <techmax@adinet.com.uy>
2025-01-30 12:20:41 -0300

    login form errror

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -76,12 +76,12 @@ export default function LoginForm() {
           aria-live="polite"
           aria-atomic="true"
         >
-          {errorMessage && (
+          {errorMessage ? (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
               <p className="text-sm text-red-500">{errorMessage}</p>
             </>
-          )}
+          ) : null}
         </div>
       </div>
     </form>
