5a2a210b18d1adff1a79401c334dbf21da64fb13
Maximiliano DUva <techmax@adinet.com.uy>
2025-01-30 17:08:28 -0300

    añadir boton logout

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -6,10 +6,12 @@ import {
     CalendarDaysIcon,
     CreditCardIcon,
     IdentificationIcon,
+    PowerIcon,
   } from '@heroicons/react/24/outline';
   import Link from 'next/link';
   import { usePathname } from 'next/navigation';
   import clsx from 'clsx';
+  import { signOut } from '../auth';
    
   const links = [
     { icon: HomeIcon, name: 'Home', href: '/dashboard' },
@@ -42,7 +44,19 @@ import {
                 </Link>
             );
             })}
-        
+
+            
+        <form
+          action={async () => {
+            'use server';
+            await signOut({ redirectTo: '/' });
+          }}
+        >
+          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form> 
       </>
     );
   }
