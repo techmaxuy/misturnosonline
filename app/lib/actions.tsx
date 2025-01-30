72602de79cf79472c8878f8b4a26aba1d1594813
Maximiliano DUva <techmax@adinet.com.uy>
2025-01-30 12:16:26 -0300

    descoemente el prevstate en actions

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -76,7 +76,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 export async function updateInvoice(
   id: string,
-  //prevState: State,
+  prevState: State,
   formData: FormData,
 ) {
   const validatedFields = UpdateInvoice.safeParse({
