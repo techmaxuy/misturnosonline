203fad076a8bd1e40d61b2eac141943ad145f025
Maximiliano DUva <techmax@adinet.com.uy>
2025-01-30 16:52:22 -0300

    ultimos cambios autenticacion

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,8 +4,8 @@ import { z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-//import { signIn } from '@/auth';
-//import  AuthError  from 'next-auth';
+import { signIn } from '../auth';
+import  {AuthError}  from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -115,20 +115,20 @@ export async function deleteInvoice(id: string) {
 }
 
 export async function authenticate(
-  //prevState: string | undefined,
-  //formData: FormData,
+  prevState: string | undefined,
+  formData: FormData,
 ) {
-  //try {
-    //await signIn('credentials', formData);
-  //} catch (error) {
-    //if (error instanceof AuthError) {
-      //switch (error.type) {
-        //case 'CredentialsSignin':
-          //return 'Invalid credentials.';
-        //default:
-         // return 'Something went wrong.';
-     // }
-    //}
-    //throw error;
-  //}
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
